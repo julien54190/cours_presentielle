f85f0fcd23141c85c4b94516338b89d30a305909
Julien Lucadello <lucadello.julien@hotmail.fr>
2023-10-02 16:25:18 +0200

    ajout media queries

---
+++ 13.Vidéo_audio/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>audio et video</title>
+</head>
+<body>
+    <audio controls autoplay loop muted src="Le Wanski - Tarte à la myrtille.mp3"></audio>
+
+    <video controls muted >
+        <source src="Hello Kiki - Lord of the Boyard - Les Kassos .mp4" type="video/mp4">
+    </video>
+
+    <iframe width='100%' height="1000px" src="https://playcanv.as/p/2OlkUaxF/" frameborder="0"></iframe>
+
+        <!-- <iframe width='50%' height="700px" src="https://playcanv.as/p/3RerJIcy/" frameborder="0"></iframe> -->
+
+            <iframe width='50%' height="700px" src="https://playcanv.as/p/XtfZXpxU/" frameborder="0"></iframe>
+
+</body>
+</html>
---
+++ 14.mediaqueries/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Media Queries</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<body>
+    <div class="grand">
+        <div class="bleu"></div>
+        <div class="violet"></div>
+        <div class="pourpre"></div>
+        <div class="chartreuse"></div>
+    </div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <!-- Quand la taille de l'écran descend en dessous de 1000px 
+    vous avez des rond qui forme un damier sinon ils sont tous bleu -->
+
+    <div class="container">
+        <div class="noir"></div>
+        <div class="blanc"></div>
+        <div class="noir"></div>
+        <div class="blanc"></div>
+        <div class="blanc"></div>
+        <div class="noir"></div>
+        <div class="blanc"></div>
+        <div class="noir"></div>
+        <div class="noir"></div>
+        <div class="blanc"></div>
+        <div class="noir"></div>
+        <div class="blanc"></div>
+        <div class="blanc"></div>
+        <div class="noir"></div>
+        <div class="blanc"></div>
+        <div class="noir"></div>
+        
+        
+        
+        </div>
+</body>
+</html>
