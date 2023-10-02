7698aa6e8e681432d41c3ecf997288946e84285e
Julien Lucadello <lucadello.julien@hotmail.fr>
2023-10-02 11:03:02 +0200

    add navigation

---
+++ 12.navigation/exercice-nav/nav_tag.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Barre Nav</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#" class="active">Contact</a></li>
+            <li><a href="#">Support</a></li>
+            
+            <li><a href="#"><span id="border"></span><box-icon name='search' color='#ffffff' ></box-icon></a></li>
+        </ul>
+    </nav>
+</body>
+</html> 
---
+++ 12.navigation/navigation.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<nav>
+<ol>
+            <li><a href="../8.exercice_Tableau/index.php">Tableau JO</a></li>
+            <li><a href="../6.flexbox/flexbox.php">FlexBox</a></li>
+            <li><a href="../7.clock/correction_minuteur/minuteur_correction.php">Clock</a></li>
+        </ol>
+    </nav>
+    <nav>
+        <ul>
+            <li>Chocolat Noir</li>
+            <li>Chocolat au lait</li>
+            <li>Chocolat Blanc</li>
+        </ul>
+    </nav>
+</body>
+</html>
