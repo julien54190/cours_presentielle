52fa89ee73aa9a5eaec7c583d89d6e7051c6bc6e
Julien Lucadello <lucadello.julien@hotmail.fr>
2023-10-04 16:26:58 +0200

    ajout nav bar et jquery

---
+++ 14.mediaqueries/exo_nav_bar2/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <script src="../../script/jquery-3.7.1.min.js
+    "></script>
+    <link rel="stylesheet" href="style.css">
+    <title>nav-bar2</title>
+</head>
+<body>
+<nav>
+        <div class="hidden">        
+            <box-icon name="world" color="#fff" size="30px"></box-icon>
+            <span>Mondial World</span>
+        </div>
+        <ul>
+            <li>About</li>
+            <li>Services</li>
+            <li>Testimonials</li>
+        </ul> 
+        <div> 
+            <box-icon name="github" type="logo" color="#fff" size="30px" class="hidden"></box-icon>
+            <box-icon name="menu" id="menu" color="#fff" size="30px" class="hidden"></box-icon>
+            <box-icon name='x' color='#fefefe' ></box-icon>
+        </div>
+    </nav>
+    <script>
+        $('#menu').click(function(){
+            if ($('nav ul').is(':visible')) {
+                $('nav ul').animate({
+                    opacity:0,
+                    
+                }, 500, function(){
+                    $('nav ul').hide()
+
+                })
+            }else {
+                $('nav ul').show()
+                $('nav ul').animate({
+                    opacity:1,
+                    left:'50px'
+                }, 500)
+
+                $('nav').css({
+                    'background-color': '#000D1A',
+                    'bottom': 0,
+                }, 500, function(){
+                    $('nav').hide()
+                })
+                $('.hidden').css({
+                    'display': 'none'
+                })
+            }
+        })
+    </script>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</body>
+</html>
